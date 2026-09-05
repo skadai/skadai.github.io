1c79f9873fc55f19c149d7cbab7c189b741ca2cb
skadai <skchang08@126.com>
2026-09-05 21:55:56 +0800

    chore: set Astro site to blog.chengshu.space for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://skadai.github.io',
+  site: 'https://blog.chengshu.space',
   base: '/',
 });
